baf238898d5d60c3527b203c50784f7db584ed22
Kunal Singh <kunalsin9h@gmail.com>
2023-10-03 10:02:21 +0530

    seperated .gql files

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
 import db from "./db";
+import loadGraphQL from "@kunalsin9h/load-gql";
+
+const typeDefs = loadGraphQL("graphql");
 
 const server = new ApolloServer({
   typeDefs,
